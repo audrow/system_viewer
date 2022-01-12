97d035c2946a8832ebb06653fff0833aa5a91c1d
Audrow Nash <audrow@hey.com>
2022-01-12 09:36:12 -0800

    Try to update variable used in DOM in onRender
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/SystemViewerPanel2.tsx
+++ src/SystemViewerPanel2.tsx
@@ -4,7 +4,7 @@ import {
   RenderState,
   Time,
 } from "@foxglove/studio";
-import { Component } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 import { StdMsgString } from "./types/StdMsgString";
@@ -15,157 +15,146 @@ type EventMessage = {
   [key: string]: string;
 };
 
-type SystemViewerProps = {
-  context: PanelExtensionContext;
-};
+function SystemViewerPanel2({ context }: { context: PanelExtensionContext }): JSX.Element {
 
-type SystemViewerState = {
-  renderDone?: (() => void);
-  previewTime?: number;
-  nodes: string[];
-};
+  // Fox extension variables
+  const [renderDone, setRenderDone] = useState<(() => void) | undefined>();
+  // const [currentFrame, setCurrentFrame] = useState<readonly MessageEvent<unknown>[] | undefined>();
+  const [allFrames, setAllFrames] = useState<readonly MessageEvent<unknown>[] | undefined>([]);
+  const [previewTime, setPreviewTime] = useState<number|undefined>();
 
-class SystemViewerPanel
-  extends Component<SystemViewerProps, SystemViewerState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      renderDone: undefined,
-      previewTime: undefined,
-      nodes: [],
-    };
-  }
+  // My state variables
+  const [nodes, setNodes] = useState<string[]>([]);
 
-  componentDidMount() {
-    this.props.context.onRender = (renderState: RenderState, done) => {
-      // on a new message try to update the current nodes
+  useLayoutEffect(() => {
+    context.onRender = (renderState: RenderState, done) => {
+      // Note if these are not updated react will bail
+      // Also, it wasn't working for me to set the nodes here - I'm not sure why
+      setAllFrames(renderState.allFrames);
+      // setCurrentFrame(renderState.currentFrame);
+      setPreviewTime(renderState.previewTime);
       if (renderState.currentFrame && renderState.currentFrame.length > 0) {
-        // Try updating nodes with current frame
-        const nodes = this.updateNodesFromMessage(
-          renderState.currentFrame,
-          this.state.nodes,
-        );
-        // Update nodes only if there is a change
-        if (nodes !== this.state.nodes) {
-          this.setState({ nodes });
-        }
+        setNodes( // react bails out if the nodes are the same
+          updateNodesFromMessage(renderState.currentFrame, nodes))
       }
-      // handle when the user picks a time with the time slider
-      this.setState({ previewTime: renderState.previewTime }, () => {
-        if (this.state.previewTime && this.props.context.seekPlayback) {
-          this.props.context.seekPlayback(this.state.previewTime);
-          const nodes = this.updateNodesToTime(
-            this.state.previewTime,
-            renderState.allFrames,
-          );
-          if (nodes !== this.state.nodes) {
-            this.setState({ nodes });
-          }
-        }
-      });
-      // necessary to call done() to finish rendering
-      this.setState({ renderDone: done });
+      setRenderDone(done);
     };
+    context.watch("currentFrame");
+    context.watch("allFrames");
+    context.watch("previewTime");
+    context.subscribe(["system_events"]);
+  }, []);
 
-    // the things that we want access to in the render state object
-    this.props.context.watch("currentFrame");
-    this.props.context.watch("allFrames");
-    this.props.context.watch("previewTime");
-    this.props.context.subscribe(["system_events"]);
-  }
+  useEffect(() => {
+    renderDone?.();
+  }, [renderDone])
 
-  private updateNodesToTime(
-    time: number,
-    allMessages: readonly MessageEvent<unknown>[] | undefined,
-  ) {
-    const timeObject = this.getTimeFromNumber(time);
-    const rawMessages = this.getAllMessagesBeforeTime(allMessages, timeObject);
-    const nodes = this.updateNodesFromMessage(rawMessages, []);
-    return nodes
-  }
+  // useEffect(() => {
+  //   if (currentFrame && currentFrame.length > 0) {
+  //     setNodes( // react bails out if the nodes are the same
+  //       updateNodesFromMessage(currentFrame, nodes))
+  //   }
+  // }, [currentFrame]);
 
-  componentDidUpdate() {
-    this.state.renderDone?.();
-  }
-
-  getTimeFromNumber(num: number): Time {
-    const seconds = Math.floor(num);
-    const nanoseconds = Math.floor((num - seconds) * 1_000_000_000);
-    return { sec: seconds, nsec: nanoseconds } as Time;
-  }
-
-  getMessageData(
-    messages: readonly MessageEvent<unknown>[] | undefined,
-  ): string[] {
-    return messages?.map((message) => (message.message as StdMsgString).data) ??
-      [];
-  }
-
-  getAllMessagesBeforeTime(
-    allMessages: readonly MessageEvent<unknown>[] | undefined,
-    time: Time,
-  ) {
-    const messages =
-      allMessages?.filter((message) =>
-        this.compareTime(message.receiveTime, time) !== 1
-      ) ?? [];
-    return messages as MessageEvent<unknown>[];
-  }
-
-  compareTime(time1: Time, time2: Time) {
-    if (time1.sec < time2.sec) {
-      return -1;
-    } else if (time1.sec > time2.sec) {
-      return 1;
-    } else {
-      if (time1.nsec < time2.nsec) {
-        return -1;
-      } else if (time1.nsec > time2.nsec) {
-        return 1;
-      } else {
-        return 0;
-      }
+  useEffect(() => {
+    if (previewTime && context.seekPlayback) {
+      context.seekPlayback(previewTime);
+      setNodes( // react bails out if the nodes are the same
+        updateNodesToTime(
+          previewTime,
+          allFrames
+        ));
     }
-  }
+  }, [previewTime])
 
-  updateNodesFromMessage(
-    rawMessages: readonly MessageEvent<unknown>[] | undefined,
-    nodes: string[],
-  ) {
-    const messages = this.getMessageData(rawMessages);
-    if (!messages) {
-      return nodes;
-    }
-    messages.forEach((message) => {
-      const parsedMessage: EventMessage = JSON.parse(message);
-      if (
-        parsedMessage.event === "create node" &&
-        !nodes.includes(parsedMessage.node!)
-      ) {
-        nodes.push(parsedMessage.node!);
-      } else if (parsedMessage.event === "destroy node") {
-        nodes = nodes.filter((node) => node !== parsedMessage.node!);
-      } else if (parsedMessage.event === "register topic") {
-        // TODO: register topic
-      }
-    });
+  useEffect(() => {
+    console.log("nodes state", nodes);
+  }, [nodes])
+
+  return (
+    <>
+      <h1>System Viewer</h1>
+      <h2>Nodes</h2>
+      <ul>
+        {nodes.map((node) => <li>{node}</li>)}
+      </ul>
+      <PreviewTime previewTime={previewTime} />
+    </>
+  )
+}
+
+function updateNodesFromMessage(
+  rawMessages: readonly MessageEvent<unknown>[] | undefined,
+  nodes: string[],
+) {
+  const messages = getMessageData(rawMessages);
+  if (!messages) {
     return nodes;
   }
+  messages.forEach((message) => {
+    const parsedMessage: EventMessage = JSON.parse(message);
+    console.log("parsedMessage", parsedMessage);
+    if (
+      parsedMessage.event === "create node" &&
+      !nodes.includes(parsedMessage.node!)
+    ) {
+      nodes.push(parsedMessage.node!);
+    } else if (parsedMessage.event === "destroy node") {
+      nodes = nodes.filter((node) => node !== parsedMessage.node!);
+    } else if (parsedMessage.event === "register topic") {
+      // TODO: register topic
+    }
+  });
+  console.log("nodes", nodes);
+  return nodes;
+}
 
-  render() {
-    return (
-      <>
-        <h1>System Viewer</h1>
-        <h2>Messages received</h2>
+function getMessageData(
+  messages: readonly MessageEvent<unknown>[] | undefined,
+): string[] {
+  return messages?.map((message) => (message.message as StdMsgString).data) ??
+    [];
+}
 
-        <h2>Nodes</h2>
-        <ul>
-          {this.state.nodes.map((node) => <li>{node}</li>)}
-        </ul>
+function updateNodesToTime(
+  time: number,
+  allMessages: readonly MessageEvent<unknown>[] | undefined,
+) {
+  const timeObject = getTimeFromNumber(time);
+  const rawMessages = getAllMessagesBeforeTime(allMessages, timeObject);
+  return updateNodesFromMessage(rawMessages, []);
+}
 
-        <PreviewTime previewTime={this.state.previewTime} />
-      </>
-    );
+function getTimeFromNumber(num: number): Time {
+  const seconds = Math.floor(num);
+  const nanoseconds = Math.floor((num - seconds) * 1_000_000_000);
+  return { sec: seconds, nsec: nanoseconds } as Time;
+}
+
+function getAllMessagesBeforeTime(
+  allMessages: readonly MessageEvent<unknown>[] | undefined,
+  time: Time,
+) {
+  const messages =
+    allMessages?.filter((message) =>
+      compareTime(message.receiveTime, time) !== 1
+    ) ?? [];
+  return messages as MessageEvent<unknown>[];
+}
+
+function compareTime(time1: Time, time2: Time) {
+  if (time1.sec < time2.sec) {
+    return -1;
+  } else if (time1.sec > time2.sec) {
+    return 1;
+  } else {
+    if (time1.nsec < time2.nsec) {
+      return -1;
+    } else if (time1.nsec > time2.nsec) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 }
 
@@ -175,7 +164,7 @@ function PreviewTime(props: { previewTime?: number }) {
 
 export function initSystemViewerPanel(context: PanelExtensionContext) {
   ReactDOM.render(
-    <SystemViewerPanel context={context} />,
+    <SystemViewerPanel2 context={context} />,
     context.panelElement,
   );
 }
