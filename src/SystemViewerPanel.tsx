6b3b32bd5c2f88fec0594b3128917fcbf34d1b0e
Audrow Nash <audrow@hey.com>
2022-06-02 10:00:25 -0500

    Add react flow
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/SystemViewerPanel.tsx
+++ src/SystemViewerPanel.tsx
@@ -9,6 +9,109 @@ import type {Node, PubSub} from './__types__/RosEntities'
 import type RosStdMsgString from './__types__/RosStdMsgsString'
 import type Statistics from './__types__/Statistics'
 
+import ReactFlow, {useEdgesState, useNodesState} from 'react-flow-renderer'
+
+// import './updatenode.css';
+
+const initialNodes = [
+  {id: '1', data: {label: '-'}, position: {x: 100, y: 100}},
+  {id: '2', data: {label: 'Node 2'}, position: {x: 100, y: 200}},
+]
+
+const initialEdges = [{id: 'e1-2', source: '1', target: '2'}]
+
+const UpdateNode = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+  const [nodeName, setNodeName] = useState('Node 1')
+  const [nodeBg, setNodeBg] = useState('#eee')
+  const [nodeHidden, setNodeHidden] = useState(false)
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.data = {
+            ...node.data,
+            label: nodeName,
+          }
+        }
+
+        return node
+      }),
+    )
+  }, [nodeName, setNodes])
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.style = {...node.style, backgroundColor: nodeBg}
+        }
+
+        return node
+      }),
+    )
+  }, [nodeBg, setNodes])
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // when you update a simple type you can just update the value
+          node.hidden = nodeHidden
+        }
+
+        return node
+      }),
+    )
+    setEdges((eds) =>
+      eds.map((edge) => {
+        if (edge.id === 'e1-2') {
+          edge.hidden = nodeHidden
+        }
+
+        return edge
+      }),
+    )
+  }, [nodeHidden, setNodes, setEdges])
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      defaultZoom={1.5}
+      minZoom={0.2}
+      maxZoom={4}
+      attributionPosition="bottom-left"
+    >
+      <div className="updatenode__controls">
+        <label>label:</label>
+        <input value={nodeName} onChange={(evt) => setNodeName(evt.target.value)} />
+
+        <label className="updatenode__bglabel">background:</label>
+        <input value={nodeBg} onChange={(evt) => setNodeBg(evt.target.value)} />
+
+        <div className="updatenode__checkboxwrapper">
+          <label>hidden:</label>
+          <input
+            type="checkbox"
+            checked={nodeHidden}
+            onChange={(evt) => setNodeHidden(evt.target.checked)}
+          />
+        </div>
+      </div>
+    </ReactFlow>
+  )
+}
+
 function SystemViewerPanel({context}: {context: PanelExtensionContext}): JSX.Element {
   const [renderDone, setRenderDone] = useState<(() => void) | undefined>()
   const [nodes, setNodes] = useState<Node[]>([])
@@ -77,6 +180,7 @@ function SystemViewerPanel({context}: {context: PanelExtensionContext}): JSX.Ele
 
   return (
     <>
+      <UpdateNode />
       <h1>System Viewer</h1>
       <h2>Nodes</h2>
       <ul>
